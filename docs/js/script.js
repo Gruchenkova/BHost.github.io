1262132c7ee5910416de6258cc37a41141cc00ca
Evgenia Gruchenkova <gruchenkovaevgenia@yandex.by>
2020-11-08 21:55:13 +0300

    added sections, fixed half of the errors

--- docs/js/script.js
+++ docs/js/script.js
@@ -54,3 +54,29 @@ $('.slider_icon').slick({
 $('.tab-title').on('click', function() {
 	$(this).siblings('.tab-content').slideToggle();
 });
+
+$('.slider_comment').slick({
+  dots: true,
+  dotsClass: "my-dots",
+  slidesToShow: 1,
+  slidesToScroll: 1,
+});
+
+
+
+// let links = document.querySelectorAll('.tabs-links li');
+// let content = document.querySelectorAll('.tabs-content li');
+// for(let i=0; i <links.length; i++) {
+//     (function(i) {
+//        let link = links[i];
+//        link.onclick = function() {
+//            for(let j=0; j <content.length; j++) {
+//               let opacity = window.getComputedStyle(content[j]).opacity;
+//               if(opacity == "1") {
+//                  content[j].style.opacity = "0";
+//               }
+//            }
+//         content[i].style.opacity = "1";
+//         }
+//     })(i);
+// }
